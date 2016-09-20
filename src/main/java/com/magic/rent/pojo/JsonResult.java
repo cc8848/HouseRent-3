d65c03a9fe6d3e507df5a5215e33c4f6a69cad4c
wuxinzhe <wuxinzhe@126.com>
2016-09-20 22:38:09 +0800

    2016年09月20日22:38:01

---
+++ src/main/java/com/magic/rent/pojo/JsonResult.java
@@ -0,0 +1,62 @@
+package com.magic.rent.pojo;
+
+/**
+ * Created by wuxinzhe on 16/9/20.
+ */
+public class JsonResult {
+
+    private Boolean status = true;
+    private String message;
+    private Object data;
+
+    public JsonResult() {
+    }
+
+    public JsonResult(Object data) {
+        this.data = data;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public JsonResult setStatus(Boolean status) {
+        this.status = status;
+        return this;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public JsonResult setMessage(String message) {
+        this.message = message;
+        return this;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public JsonResult setData(Object data) {
+        this.data = data;
+        return this;
+    }
+
+    public static JsonResult success() {
+        return new JsonResult().setStatus(true);
+    }
+
+    public static JsonResult success(Object data) {
+        return success().setData(data);
+    }
+
+    public static JsonResult error() {
+        return new JsonResult().setStatus(false);
+    }
+
+    public static JsonResult error(String message) {
+        return error().setMessage(message);
+    }
+
+}
