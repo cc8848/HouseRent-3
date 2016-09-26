740585a5f9cc831b97a721053dc78312a8d7aca2
wuxinzhe-MacbookAir <wuxinzhe@126.com>
2016-09-26 17:01:18 +0800

    2016年09月26日17:01:12

---
+++ src/main/java/com/magic/rent/controller/JCaptchaController.java
@@ -0,0 +1,52 @@
+package com.magic.rent.controller;
+
+import com.magic.rent.util.Log;
+import com.octo.captcha.service.image.ImageCaptchaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+/**
+ * 知识产权声明:本文件自创建起,其内容的知识产权即归属于原作者,任何他人不可擅自复制或模仿.
+ * 创建者: wu   创建时间: 16/9/26
+ * 类说明:
+ */
+@Controller
+public class JCaptchaController {
+
+    @Autowired
+    private ImageCaptchaService imageCaptchaService;
+
+    @RequestMapping("/getCaptchaImage")
+    public void getCaptchaImage(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            ByteArrayOutputStream jpegOutputStream = new ByteArrayOutputStream();
+            String captchaId = request.getSession().getId();
+            BufferedImage challenge = imageCaptchaService.getImageChallengeForID(captchaId, request.getLocale());
+
+            response.setHeader("Cache-Control", "no-store");
+            response.setHeader("Pragma", "no-cache");
+            response.setDateHeader("Expires", 0L);
+            response.setContentType("image/jpeg");
+
+            ImageIO.write(challenge, "jpeg", jpegOutputStream);
+            byte[] captchaChallengeAsJpeg = jpegOutputStream.toByteArray();
+
+            ServletOutputStream respOs = response.getOutputStream();
+            respOs.write(captchaChallengeAsJpeg);
+            respOs.flush();
+            respOs.close();
+        } catch (IOException e) {
+            Log.error(JCaptchaController.class, "获取验证码", e.getMessage());
+        }
+    }
+
+}
