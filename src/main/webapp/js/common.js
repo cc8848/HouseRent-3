7b488a36e920909e61b555474330285919562a6b
wuxinzhe <wxuinzhe@126.com>
2016-11-26 12:59:23 +0800

    2016年11月26日12:59:14

--- src/main/webapp/js/common.js
+++ src/main/webapp/js/common.js
@@ -1,6 +1,40 @@
 /**
  * Created by wuxinzhe on 16/10/7.
  */
+function upLoad(fileInput, URL) {
+
+    var fileInput = fileInput;
+
+    var URL = URL;
+
+    this.initParameter = {
+        language: 'zh',//语言
+        uploadUrl: URL,//上传的地址
+        allowedFileExtensions: ['jpg', 'gif', 'png'],//接收的文件后缀
+        showUpload: true, //是否显示上传按钮
+        showCaption: false,//是否显示标题
+        browseClass: "btn btn-primary", //按钮样式
+        //dropZoneEnabled: false,//是否显示拖拽区域
+        //minImageWidth: 50, //图片的最小宽度
+        //minImageHeight: 50,//图片的最小高度
+        //maxImageWidth: 1000,//图片的最大宽度
+        //maxImageHeight: 1000,//图片的最大高度
+        //maxFileSize: 0,//单位为kb，如果为0表示不限制文件大小
+        //minFileCount: 0,
+        maxFileCount: 5, //表示允许同时上传的最大文件个数
+        enctype: 'multipart/form-data',
+        validateInitialCount: true,
+        // previewFileIcon: "<i class='glyphicon glyphicon-king'></i>",
+        msgFilesTooMany: "选择上传的文件数量({n}) 超过允许的最大数值{m}！"
+    };
+
+    this.init = function () {//控件初始化
+        fileInput.fileinput(this.initParameter);
+    }
+
+}
+
+
 function Modal() {
     this.errorModal = function (message, tittle) {
         if (arguments.length == 2) {
@@ -30,13 +64,31 @@ function Modal() {
             radioClass: 'iradio_flat-red'
         });
         //初始化文件上传组件
-        $('.file-input').fileinput();
+        $('#file').fileinput({
+            language: 'zh',//语言
+            uploadUrl: '/file/upload',//上传的地址
+            initialCaption: '请上传房屋图片...',//文本框初始化标题
+            //allowedFileTypes:['image'],//允许上传的文件类型
+            allowedFileExtensions: ['jpg'],//接收的文件后缀
+            //showUpload: true, //是否显示上传按钮
+            showCaption: true,//是否显示标题
+            dropZoneEnabled: true,//是否显示拖拽区域
+            //minImageWidth: 50, //图片的最小宽度
+            //minImageHeight: 50,//图片的最小高度
+            //maxImageWidth: 1000,//图片的最大宽度
+            //maxImageHeight: 1000,//图片的最大高度
+            maxFileSize: 1024,//单位为kb，如果为0表示不限制文件大小
+            maxFileCount: 2,//表示允许同时上传的最大文件个数
+            validateInitialCount: true,
+            overwriteInitial: false,
+            enctype: 'multipart/form-data',
+            msgFilesTooMany: "选择上传的文件数量({n}) 超过允许的最大数值{m}"
+        });
 
     }
 }
 
 function SelectUtil() {
-    var _this = this;
     this.selectInit = function (target, URL) {
         $.getJSON(URL, function (backData) {
             target.select2({
@@ -66,3 +118,4 @@ function postCRF(URL, data, callback) {
         }
     });
 }
+
